2f67d7a154e13e8fa8a5f8f07460bde9e01ed10b
Johan Esteves <jsesteves91@gmail.com>
2017-10-10 00:20:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,10 +2,15 @@ class PigLatinize
   attr_reader :text
 
   def initialize(text)
-    @text = text
+    @text = text.upcase
   end
 
   def firstletter
     self.text[0]
+  end
+
+  def latinize
+    vowels = ["A","E", I, O, U]
+    if
   end
 end
