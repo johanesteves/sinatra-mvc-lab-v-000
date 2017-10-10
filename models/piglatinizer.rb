c115a0c73566a536b27c5df28a5974dd4c127413
Johan Esteves <jsesteves91@gmail.com>
2017-10-10 00:30:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinize
       result_text = self.text + "way"
     else
       split_text = self.text.split("")
-      split_text.detect.with_index
+      split_text.index {|letter| vowels.include(letter) }
     end
   end
 end
