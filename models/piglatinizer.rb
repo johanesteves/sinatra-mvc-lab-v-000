3254b4fce968de4dd59d1e1864df2ba5f3a71466
Johan Esteves <jsesteves91@gmail.com>
2017-10-10 00:46:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,11 +12,12 @@ class PigLatinize
   def latinize
     vowels = ["A","E", "I", "O", "U"]
 
-    if vowels.include?(self.firstletter)
-      result_text = self.text + "way"
+    if vowels.include?(self.firstleter)
+      result_text = self.text + "WAY"
     else
       split_text = self.text.split("")
       vowel_index = split_text.index {|letter| vowels.include?(letter) }
+
       result_text = split_text.rotate(vowel_index).join("") + "AY"
     end
   end
